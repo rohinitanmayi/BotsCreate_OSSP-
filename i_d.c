1fca74a152c86c7e272b25683a742c76263a78de
rohinitanmayi <rohinitanmayidusi@gmail.com>
2026-09-16 06:07:39 +0000

    Add i_d memory allocation program

---
+++ i_d.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int *p;
+
+    printf("enter the size of dynamic memory\n");
+    scanf("%d", &n);
+
+    p = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d element\n", (i + 1));
+        scanf("%d", p + i);
+    }
+
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d element is: %d\n", (i + 1), *(p + i));
+    }
+
+    // Increase memory
+    p = (int *)realloc(p, (n + 2) * sizeof(int));
+
+    p[n] = 5;
+    p[n + 1] = 10;
+
+    printf("\nAfter increasing memory:\n");
+
+    int j;
+    for (j = 0; j < n + 2; j++)
+    {
+        printf("%d element is: %d\n", (j + 1), *(p + j));
+    }
+
+    // Decrease memory
+    p = (int *)realloc(p, (n - 2) * sizeof(int));
+
+    printf("\nAfter decreasing memory:\n");
+
+    for (j = 0; j < n - 2; j++)
+    {
+        printf("%d element is: %d\n", (j + 1), *(p + j));
+    }
+
+    free(p);
+
+    return 0;
+}
